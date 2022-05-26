4f64ab59acf79c1801f488dbf6eeab6fd49fa6bf
gpeiris <56284756+gpeiris@users.noreply.github.com>
2022-05-26 16:15:10 +1000

    Update QIvVolume.py

--- QIvVolume.py
+++ QIvVolume.py
@@ -2,12 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-col = 'Bladder_V30' # Pick a Quality Indicator from the list below
+site = 'LUNG' # Pick an anatomical site from {PROSTATE, LUNG}
+col  = 'CI'   # Pick a Quality Indicator from the list below matching the selected site
 
-QualIndi = ['Volume','CI','HI','CTV_D98','PTV_D2','PTV_D1','PTV_D99','PTV_D98','Bladder_V60','Bladder_V50',
+if site == 'PROSTATE':
+    Patients = ['004','005','006','007','008','011','002','010','001','003','009']  # Patient order based on order of appearance in .csv
+    QualIndi = ['Volume','CI','HI','CTV_D98','PTV_D2','PTV_D1','PTV_D99','PTV_D98','Bladder_V60','Bladder_V50',
             'Bladder_V45','Bladder_V40','Bladder_V30','Rectum_V60','Rectum_V57','Rectum_V50','Rectum_V45',
                              'Rectum_V35','Rectum_V30','Rectum_V25','Rectum_V20']
-Patients = ['004','005','006','007','008','011','002','010','001','003','009']    # Patient order based on order of appearance in .csv
+    C_col    = 33
+    dataFile = 'ProstateData.csv'
+if site == 'LUNG':
+    Patients = ['002','003','004','005','006','007','008','009','010','011','001']
+    QualIndi = ['Volume','CI','HI','CTV_D100','PTV_D2','PTV_D98','BPlexL_D0','BPlexR_0','Heart_V50','Lung_V20',
+            'Lung_V5','Lung_V30','Oesophagus_D0']
+    C_col    = 26
+    dataFile = 'LungData.csv'
 
 #  Set up dataFrames
 df_25mm = pd.DataFrame(columns= QualIndi, index= Patients) #   2.5mm Leaf Width Quality Indicators
@@ -15,7 +25,7 @@ df_5mm  = pd.DataFrame(columns= QualIndi, index= Patients) #     5mm Leaf Width 
 df_10mm = pd.DataFrame(columns= QualIndi, index= Patients) #    10mm Leaf Width Quality Indicators
 df_clin = pd.DataFrame(columns= QualIndi, index= Patients) # Clinically Planned Quality Indicators
 
-#  Sort prostate data into dataFrames
+#  Sort patient data into dataFrames
 with open('ProstateData.csv', 'r') as file:
     data = file.read().splitlines()
     for pat in Patients:             # Loop through each Patient
@@ -46,14 +56,14 @@ c = [0,0,0] # Y-Intercept array
 plt.figure(figsize=(12,8))
 ax = plt.gca()
 for i in range(3):  # Loop through each leaf width
-    df[i].plot('Volume',col, ax=ax, kind='scatter',c=color[i],s=50). #  Scatter plot QI against Volume
+    df[i].plot('Volume',col, ax=ax, kind='scatter',c=color[i],s=50) #  Scatter plot QI against Volume
     x = list(df[i]['Volume'])
     z = np.polyfit(x,list(df[i][col]), 1) # Calulate trendline of data
     p = np.poly1d(z)
     plt.plot(x,p(x),color[i])  #  plot trendline of QI against Volume
     m[i], c[i] = z[0],z[1]
-
-plt.title('Volume v '+col,**font,fontsize=title_size)
+            
+plt.title('Volume v '+col+" for Different MLC Leaf Widths for "+site+" Patients",**font,fontsize=title_size)
 plt.xlabel('Volume [cc]',**font,fontsize=label_size)
 plt.ylabel(col.replace("_",' ')+' [%]',**font,fontsize=label_size)
 plt.legend(['2.5mm',"%.4fV+(%.2f)"%(m[0],c[0]),'5mm',"%.4fV+(%.2f)"%(m[1],c[1]),'10mm',"%.4fV+(%.2f)"%(m[2],c[2])])
