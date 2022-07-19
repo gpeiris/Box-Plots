b6cf599bb67438da1cb9937a0628f84a5426c9a2
gpeiris <56284756+gpeiris@users.noreply.github.com>
2022-07-19 16:22:45 +1000

    Update QIvVolume.py

--- QIvVolume.py
+++ QIvVolume.py
@@ -2,8 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+font    = {'fontname':'times new roman'}
+title_size = 20
+label_size = 14
+
+m = [0,0,0] # Gradient array
+c = [0,0,0] # Y-Intercept array
+
 site = 'LUNG' # Pick an anatomical site from {PROSTATE, LUNG}
-col  = 'CI'   # Pick a Quality Indicator from the list below matching the selected site
 
 if site == 'PROSTATE':
     Patients = ['004','005','006','007','008','011','002','010','001','003','009']  # Patient order based on order of appearance in .csv
@@ -26,45 +32,41 @@ df_10mm = pd.DataFrame(columns= QualIndi, index= Patients) #    10mm Leaf Width 
 df_clin = pd.DataFrame(columns= QualIndi, index= Patients) # Clinically Planned Quality Indicators
 
 #  Sort patient data into dataFrames
-with open('ProstateData.csv', 'r') as file:
+with open(dataFile, 'r') as file:
     data = file.read().splitlines()
     for pat in Patients:             # Loop through each Patient
         i = Patients.index(pat)
-        QI2 = data[4+i].split(',')
-        QI5 = data[18+i].split(',')
-        QI10 = data[32+i].split(',') 
-        QIC = data[4+i].split(',')
+        QI2 = data[1+3*i].split(',')
+        QI5 = data[2+3*i].split(',')
+        QI10 = data[3+3*i].split(',')
+        #QIC = data[4+i].split(',')
         for qi in QualIndi:          # Loop through each Quality Indicator
             j = QualIndi.index(qi)
-            df_25mm.loc[pat][qi] = float(QI2[11+j])
-            df_5mm.loc[pat][qi] = float(QI5[11+j])
-            df_10mm.loc[pat][qi] = float(QI10[11+j])
-            df_clin.loc[pat][qi] = float(QIC[33+j])
+            df_25mm.loc[pat][qi] = float(QI2[4+j])
+            df_5mm.loc[pat][qi] = float(QI5[4+j])
+            df_10mm.loc[pat][qi] = float(QI10[4+j])
+            #df_clin.loc[pat][qi] = float(QIC[C_col+j])
 
 
 df = [df_25mm,df_5mm,df_10mm] #  Group Leaf Width Data Frames in a list
 color = ['steelblue','limegreen','indianred']
 
-font    = {'fontname':'times new roman'}
-title_size = 20
-label_size = 14
-
-m = [0,0,0] # Gradient array
-c = [0,0,0] # Y-Intercept array
-
-# Plot of selected Quality Indicator against Volume
-plt.figure(figsize=(12,8))
-ax = plt.gca()
-for i in range(3):  # Loop through each leaf width
-    df[i].plot('Volume',col, ax=ax, kind='scatter',c=color[i],s=50) #  Scatter plot QI against Volume
-    x = list(df[i]['Volume'])
-    z = np.polyfit(x,list(df[i][col]), 1) # Calulate trendline of data
-    p = np.poly1d(z)
-    plt.plot(x,p(x),color[i])  #  plot trendline of QI against Volume
-    m[i], c[i] = z[0],z[1]
+for col in QualIndi:
+    # Plot of selected Quality Indicator against Volume
+    plt.figure(figsize=(12,8))
+    ax = plt.gca()
+    for i in range(3):  # Loop through each leaf width
+        df[i].plot('Volume',col, ax=ax, kind='scatter',c=color[i],s=50) #  Scatter plot QI against Volume
+        x = list(df[i]['Volume'])
+        z = np.polyfit(x,list(df[i][col]), 1) # Calulate trendline of data
+        p = np.poly1d(z)
+        plt.plot(x,p(x),color[i])  #  plot trendline of QI against Volume
+        m[i], c[i] = z[0],z[1]
             
-plt.title('Volume v '+col+" for Different MLC Leaf Widths for "+site+" Patients",**font,fontsize=title_size)
-plt.xlabel('Volume [cc]',**font,fontsize=label_size)
-plt.ylabel(col.replace("_",' ')+' [%]',**font,fontsize=label_size)
-plt.legend(['2.5mm',"%.4fV+(%.2f)"%(m[0],c[0]),'5mm',"%.4fV+(%.2f)"%(m[1],c[1]),'10mm',"%.4fV+(%.2f)"%(m[2],c[2])])
-plt.show()
+    plt.title('Volume v '+col+" for Different MLC Leaf Widths for "+site+" Patients",**font,fontsize=title_size)
+    plt.xlabel('Volume [cc]',**font,fontsize=label_size)
+    plt.ylabel(col.replace("_",' ')+' [%]',**font,fontsize=label_size)
+    plt.legend(['2.5mm',"%.4fV+(%.2f)"%(m[0],c[0]),'5mm',"%.4fV+(%.2f)"%(m[1],c[1]),'10mm',"%.4fV+(%.2f)"%(m[2],c[2])])
+    plt.savefig('outputFigures/Volume/' + site + '_' + col + '.png', dpi=600, transparent=True)
+    plt.show()
+    plt.close()
